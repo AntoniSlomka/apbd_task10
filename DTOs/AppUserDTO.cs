d4b18f5b3fd5a52f1753080a4a346e3b10f3b87a
Antoni Słomka <s33610@pjwstk.edu.pl>
2026-05-31 21:29:32 +0200

    Adde the /Admin page

---
+++ DTOs/AppUserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace apbdtask10_v2.DTOs
+{
+    public class AppUserDTO
+    {
+        public string Username { get; set; } = null!;
+        public int NoteCout { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
