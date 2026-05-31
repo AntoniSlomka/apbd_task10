7fa3d68c41912f723bae12297bd78c165424a178
Antoni Słomka <s33610@pjwstk.edu.pl>
2026-05-31 18:40:54 +0200

    Added Login page

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace apbdtask10_v2.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
