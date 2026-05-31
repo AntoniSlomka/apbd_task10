b6edf9a72442aa569d4b5f3cdb84a0b4746321cd
Antoni Słomka <s33610@pjwstk.edu.pl>
2026-05-31 18:56:38 +0200

    Added the register page and register method

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using apbdtask10_v2.Models;
+using apbdtask10_v2.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace apbdtask10_v2.Controllers
 {
     public class AccountController : Controller
     {
+
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManaager;
+
+        public AccountController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManaager)
+        {
+            _signInManager = signInManager;
+            _userManaager = userManaager;
+        }
+
         public IActionResult Login()
         {
             return View();
@@ -12,6 +25,35 @@ namespace apbdtask10_v2.Controllers
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser
+                {
+                    UserName = model.Username, 
+                    Email = model.Email
+                };
+
+                var result = await _userManaager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                    return View(model);
+                }
+            }
+            return View(model);
         }
     }
 }
